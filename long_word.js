84da88a0920d9631031b00e9a1310daae4878eda
milleriishaun <milleriishaun@gmail.com>
2018-05-22 16:47:31 -0700

    Edit long_word

--- long_word.js
+++ long_word.js
@@ -1,14 +1,15 @@
 function findLongestWord(str) {
   var array1 = str.split(" ");
   for (i = 0; i < array1.length; i++) {
-    for (j = 1; j < array1.length; j++) {
-      if (array1[j].length>array1[j-1].length) {
-        longWord = array1[j];
-        console.log(j + " : " + longWord + " : " + longWord.length);
+    for (j = 0; j < array1.length; j++) {
+      if (array1[i].length<array1[j].length) {
+        temp = array1[i];
+        array1[i] = array1[j];
+        array1[j] = temp;
       }
     }
   }
-  return console.log(longWord.length);
+  return console.log(array1[array1.length-1].length);
 }
 
-findLongestWord("The quick brown fox jumped over the lazy dog");
+
